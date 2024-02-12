cf2be2ddb0ef6d2d28e61d2408d649ec670c1026
Viktoriya Baranovskaya <vp.vika-1991@yandex.ru>
2024-02-13 00:20:16 +0300

    Premain20

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,7 @@
 /* КОНФИГ */
 const preloaderWaitindTime = 1200;
 const cardsOnPage = 5;
-const BASE_URL = 'https://v-content.practicum-team.ru1';
+const BASE_URL = 'https://v-content.practicum-team.ru';
 const endpoint = `${BASE_URL}/api/videos?pagination[pageSize]=${cardsOnPage}&`;
 
 /* ЭЛЕМЕНТЫ СТРАНИЦЫ */
